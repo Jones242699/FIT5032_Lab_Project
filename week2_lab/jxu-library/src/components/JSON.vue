da264eb87b4684f3f93ed7cb0eb4e5903008bece
Junhong Xu <jxuu0216@student.monash.edu>
2025-08-05 21:04:05 +1000

    feat(activity2): filter authors born after 1850 using computed property

--- week2_lab/jxu-library/src/components/JSON.vue
+++ week2_lab/jxu-library/src/components/JSON.vue
@@ -95,9 +95,10 @@ import bookstores from "../assets/json/bookstores.json"
 const showMessage = ref(false)
 
 // Activity 2: Get authors born after 1850
-const modernAuthors = computed(() => {
+const modernAuthors = computed(() => 
   // TODO: CODE TO FILTER ARRAY OF AUTHORS HERE
-})
+  authors.filter((author) => author.birthYear > 1850)
+)
 
 // Activity 3: Get all famous works
 const allFamousWorks = computed(() => {
