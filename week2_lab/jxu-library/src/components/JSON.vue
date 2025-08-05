7bc71c78a977397dbf7bf419afa750383bc278f4
Junhong Xu <jxuu0216@student.monash.edu>
2025-08-05 22:47:35 +1000

    feat(activity10): iterate and display storeTypes from bookstores object using v-for

--- week2_lab/jxu-library/src/components/JSON.vue
+++ week2_lab/jxu-library/src/components/JSON.vue
@@ -78,6 +78,11 @@
       <p>Store Types:</p>
       <!-- Activity 10: Iterate through the storeTypes array and display the store type and the number of stores that use that type. -->
       <!-- TODO: CODE TO RENDER LIST OF STORE TYPES HERE -->
+      <ul>
+        <li v-for="(count, type) in bookstores.storeTypes" :key="type">
+          {{ type }}: {{ count }} stores
+        </li>
+      </ul>
 
       <h3>Nested Objects</h3>
       <p>Opening Hours:</p>
