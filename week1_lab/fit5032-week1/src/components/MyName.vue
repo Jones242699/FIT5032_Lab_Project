d20c1daf2234af71bf7764d28770e9a1e469cbda
Junhong Xu <jxuu0216@student.monash.edu>
2025-08-05 19:08:22 +1000

    feat(week1): add MyName.vue component to display costomized name

---
+++ week1_lab/fit5032-week1/src/components/MyName.vue
@@ -0,0 +1,15 @@
+// MyName.vue
+<template>
+    <div class="my-name">
+        <p>Hello, my name is <strong>Lily Xu</strong>.</p>
+    </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.my-name {
+    font-size: 1.2rem; 
+    color: #42b983;
+}
+</style>
