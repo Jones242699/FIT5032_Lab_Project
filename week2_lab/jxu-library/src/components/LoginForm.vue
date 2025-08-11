7d75da42a8c9ea6be8a5f173630ff1ceb49c93e9
Junhong Xu <jxuu0216@student.monash.edu>
2025-08-12 02:11:37 +1000

    feat(lab3-activity5): refactor LoginForm.vue with Bootstrap 5 grid and form classes, add v-model bindings and submit/clear handlers

--- week2_lab/jxu-library/src/components/LoginForm.vue
+++ week2_lab/jxu-library/src/components/LoginForm.vue
@@ -1,27 +1,52 @@
 <template>
-    <div class="form">
-    <h1>User Information Form / Credentials</h1>
-        <form>
-            <label for="username">Username:</label><br>
-            <input type="text" id="username" name="username"><br>
-            
-            <label for="password">Password:</label><br>
-            <input type="password" id="password" name="password"><br>
-            
-            <label for="isAustralian">Australian Resident?</label><br>
-            <input type="checkbox" id="isAustralian" name="isAustralian"><br>
-            
-            <label for="reason">Reason For Joining:</label><br>
-            <textarea id="reason" name="reason" rows="3"></textarea><br>
-            
-            <label for="gender">Gender</label><br>
-            <select id="gender">
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col-md-8 offset-md-2">
+        <h1 class="text-center">User Information Form</h1>
+
+        <form @submit.prevent="submitForm">
+          <div class="row mb-3">
+            <div class="col-md-6">
+              <label for="username" class="form-label">Username</label>
+              <input id="username" type="text" class="form-control" v-model="formData.username" />
+            </div>
+            <div class="col-md-6">
+              <label for="password" class="form-label">Password</label>
+              <input id="password" type="password" class="form-control" v-model="formData.password" />
+            </div>
+          </div>
+
+          <div class="row mb-3">
+            <div class="col-md-6">
+              <div class="form-check mt-4">
+                <input id="isAustralian" class="form-check-input" type="checkbox" v-model="formData.isAustralian" />
+                <label class="form-check-label" for="isAustralian">Australian Resident?</label>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <label for="gender" class="form-label">Gender</label>
+              <select id="gender" class="form-select" v-model="formData.gender">
                 <option value="female">Female</option>
                 <option value="male">Male</option>
                 <option value="other">Other</option>
-            </select>
+              </select>
+            </div>
+          </div>
+
+          <div class="mb-3">
+            <label for="reason" class="form-label">Reason for joining</label>
+            <textarea id="reason" class="form-control" rows="3" v-model="formData.reason"></textarea>
+          </div>
+
+          <div class="text-center">
+            <button type="submit" class="btn btn-primary me-2">Submit</button>
+            <button type="button" class="btn btn-secondary" 
+                @click="clearForm">Clear</button>
+          </div>
         </form>
+      </div>
     </div>
+  </div>
 </template>
 
 <script setup>
