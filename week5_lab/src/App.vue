2571123100485a2117343dcc8713d0e259466db5
Junhong Xu <jxuu0216@student.monash.edu>
2025-09-01 15:53:22 +1000

    lab5-style(activity5.5.8): move container styles to App.vue for global consistency and center About page content

--- week5_lab/src/App.vue
+++ week5_lab/src/App.vue
@@ -9,12 +9,20 @@ import BHeader from './components/BHeader.vue'
     <BHeader />
   </header>
 
-  <main>
+  <main class="container">
     <router-view></router-view>
   </main>
 </template>
 
 <style scoped>
+.container {
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  max-width: 80vw;
+  margin: 0 auto;
+  padding: 20px;
+  /* background-color: #e0bfbf; */
+  border-radius: 10px;
+}
 /* header {
   line-height: 1.5;
 }
